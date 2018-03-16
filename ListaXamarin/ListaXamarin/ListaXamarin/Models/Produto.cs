8d2fb048c09716963646a088c167d6a4c5561ac7
will brigida <will_brigida@hotmail.com>
2018-03-16 15:19:14 -0300

    Inclusão da BaseViewModel

--- ListaXamarin/ListaXamarin/ListaXamarin/Models/Produto.cs
+++ ListaXamarin/ListaXamarin/ListaXamarin/Models/Produto.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace ListaXamarin.Models
 {
-    class Produto
+    public class Produto
     {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public int QuantidadeDeItens { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"ID do Produto: {this.IdProduto}\n" +
+                $"Nome do Produto: {this.NomeProduto}\n" +
+                $"Quantidade de Itens: {this.QuantidadeDeItens}" ;
+        }
+
     }
 }
---
+++ ListaXamarin/ListaXamarin/ListaXamarin/ViewModels/BaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ListaXamarin.Models
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void Notificar([CallerMemberName] string prop = null)
+
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string prop = null)
+
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+
+            {
+                return false;
+            }
+
+            storage = value;
+
+            Notificar(prop);
+
+            return true;
+        }
+    }
+}
