a41c0b3afd7ebcacfedd031e0628074a49bcf2e1
will brigida <will_brigida@hotmail.com>
2018-03-16 16:13:45 -0300

    Inclusão nuget MultiSelectObservableCollection

---
+++ ListaXamarin/ListaXamarin/ListaXamarin/ViewModels/ProdutosViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ListaXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.MultiSelectListView;
+
+namespace ListaXamarin.ViewModels
+{
+    public class ProdutosViewModel : BaseViewModel
+    {
+        public MultiSelectObservableCollection<Produto> Produtos { get; set; }
+
+        public ProdutosViewModel()
+        {
+            Produtos = new MultiSelectObservableCollection<Produto>
+            {
+                new Produto{IdProduto = 1001, NomeProduto = "Arroz", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1002, NomeProduto = "Feijão", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1003, NomeProduto = "Farinha", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1004, NomeProduto = "Macarrão", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1005, NomeProduto = "Leite", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1006, NomeProduto = "Açucar", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1007, NomeProduto = "Sal", QuantidadeDeItens = 0 },
+                new Produto{IdProduto = 1008, NomeProduto = "Óleo", QuantidadeDeItens = 0 },
+            
+            };
+        }
+    }
+}
+
