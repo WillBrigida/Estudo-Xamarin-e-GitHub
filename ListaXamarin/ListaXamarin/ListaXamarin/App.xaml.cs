520960fda772d32a767ed39d349c113d3709c5cd
will brigida <will_brigida@hotmail.com>
2018-03-18 14:38:21 -0300

    Tela de Login  e ViewModel referent

--- ListaXamarin/ListaXamarin/ListaXamarin/App.xaml.cs
+++ ListaXamarin/ListaXamarin/ListaXamarin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListaXamarin.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace ListaXamarin
 		{
 			InitializeComponent();
 
-			MainPage = new NavigationPage( new MainPage());
+			MainPage = new NavigationPage( new LoginView());
 		}
 
 		protected override void OnStart ()
---
+++ ListaXamarin/ListaXamarin/ListaXamarin/ViewModels/LoginViewModel.cs
@@ -0,0 +1,101 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace ListaXamarin.ViewModels
+{
+    public class LoginViewModel : Models.BaseViewModel
+    {
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { this.SetProperty( ref email , value); }
+        }
+
+        private string senha;
+
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                if (this.senha != value)
+                {
+                    this.SetProperty(ref senha, value);
+                }
+            }
+        }
+
+        private bool isEnabled;
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set {this.SetProperty( ref isEnabled , value); }
+        }
+
+        private bool isRunning;
+
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set {this.SetProperty(ref isRunning , value); }
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Ops!",
+                    "Você esqueceu de digitar seu Email",
+                    "Ok");
+                return;
+            }
+
+
+
+            if (string.IsNullOrEmpty(this.Senha))
+            {
+
+                await Application.Current.MainPage.DisplayAlert(
+                "Ops!",
+                "Você esqueceu de digitar sua senha",
+                "Ok");
+
+                return;
+            }
+
+
+
+
+            if (this.Email != "will_brigida@hotmail.com" || this.Senha != "123456")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Ops!",
+                    "Login e/ou Senha inválidos! Verifique os dados informados e tente novamente",
+                    "Ok");
+                return;
+            }
+        }
+
+        public LoginViewModel()
+        {
+            this.IsEnabled = true;
+        }
+    }
+}
